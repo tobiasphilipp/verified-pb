db11415c75fdd10ac8763197ae0da0ce1163af37
Anna <belpku001@mail.ru>
2017-03-23 01:15:47 +0100

    update normalizer checker

--- normalizer_check.py
+++ normalizer_check.py
@@ -11,6 +11,19 @@
 from subprocess import PIPE, Popen
 import os
 
+def check_norm(constr):
+    opers = constr.split(' ')[:-1]
+    xes = set()
+    rhs = int(constr.strip('\r\n').split(' ')[-1][2:])
+    for op in opers:
+        coef = int(op.split('*')[0])
+        x = op.split('*')[1]
+        assert coef >= 0
+        assert coef > rhs
+        assert x not in xes
+        xes.add(x)
+    return constr
+
 bench_dir = 'bench/'
 normal_dir = 'bench_normal/'
 faulty = []
@@ -24,13 +37,14 @@ for entry in os.listdir(bench_dir):
         line = f.readline()
     while len(line) > 0:
         feed = line.replace(' x','*').replace(' >= ','<').replace(' ;','')
+        print(feed)
         #answer = p.communicate('2*1 -3*2<3\n')[0]
         p = Popen(["Normalizer"], stdin=PIPE, stdout=PIPE, bufsize=1)
         answer = p.communicate(feed)[0]
-        normals += answer
+        normals += check_norm(answer)
         line = f.readline()
     f_out = open(normal_dir + entry, 'w')
-    f_out.writelines(normals)
+    f_out.writelines([x.strip('\n') for x in normals])
     f.close()
     f_out.close()
     break
